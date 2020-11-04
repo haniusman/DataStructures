06ab9ea5188862d395d7914c4d4b2aa690c3640d
haniusman <haniusman348@gmail.com>
2020-11-04 14:06:10 +0400

    Create TreeTraversals.py

---
+++ TreeTraversals.py
@@ -0,0 +1,60 @@
+class Node:
+    
+    def __init__ (self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+    
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+            
+    def inorderTraversal(self, root):
+        res = []
+        if root:
+            res = self.inorderTraversal(root.left)
+            res.append(root.data)
+            res = res + self.inorderTraversal(root.right)
+        
+        return res
+        
+    def preorderTraversal(self, root):
+       
+        res = []
+        if root:
+            
+            res.append(root.data)
+            res = res + self.preorderTraversal(root.left)
+            res = res + self.preorderTraversal(root.right)
+        return res
+        
+    def postorderTraversal(self, root):
+        res = []
+        if root:
+            res = self.postorderTraversal(root.right)
+            res.append(root.data)
+            res = res + self.postorderTraversal(root.left)
+        return res
+        
+root = Node(27)
+root.insert(19)
+root.insert(14)
+root.insert(10)
+root.insert(35)
+root.insert(42)
+root.insert(31)
+
+print(root.preorderTraversal(root))
+print(root.inorderTraversal(root))
+print(root.postorderTraversal(root))
